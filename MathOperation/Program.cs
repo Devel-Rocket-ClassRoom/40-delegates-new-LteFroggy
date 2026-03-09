2f75e4128ad7e4d401377bd60d1019d96e746c1f
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-09 17:39:22 +0900

    MathOperation 완료

--- MathOperation/Program.cs
+++ MathOperation/Program.cs
@@ -1,4 +1,31 @@
 using System;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+int a = 20, b = 4;
+
+Console.WriteLine($"=== 사칙연산 대리자 ===");
+Console.WriteLine();
+MathOp op = Add;
+Console.WriteLine($"[덧셈]");
+Console.WriteLine($"{a} + {b} = {op(a, b)}");
+Console.WriteLine();
+op = Subtract;
+Console.WriteLine($"[덧셈]");
+Console.WriteLine($"{a} - {b} = {op(a, b)}");
+Console.WriteLine();
+op = Multiply;
+Console.WriteLine($"[곱셈]");
+Console.WriteLine($"{a} * {b} = {op(a, b)}");
+Console.WriteLine();
+op = Divide;
+Console.WriteLine($"[나눗셈]");
+Console.WriteLine($"{a} / {b} = {op(a, b)}");
+Console.WriteLine();
+Console.WriteLine();
+
+
+int Add(int x, int y) => x + y;
+int Subtract(int a, int b) => a - b;
+int Multiply(int a, int b) => a * b;
+int Divide(int a, int b) => a / b;
+
+delegate int MathOp(int x, int y);
