5b5aefc589845594340c0f3f8a5c16c2033419f4
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-09 17:39:32 +0900

    StringProcessor 완료

--- StringProcessor/Program.cs
+++ StringProcessor/Program.cs
@@ -1,4 +1,45 @@
 using System;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+string ToUpperCase(string s) => s.ToUpper();
+string ToLowerCase(string s) => s.ToLower();
+string AddBrackets(string s) => "[" + s + "]";
+string Reverse(string s) {
+    char[] chars = s.ToCharArray();
+    Array.Reverse(chars);
+    return new string(chars);
+}
+
+void ProcessAndPrint(string input, StringProcessor processor) {
+    Console.WriteLine($"결과 : {processor(input)}");
+}
+
+Console.WriteLine($"=== 문자열 가공기 ===");
+Console.WriteLine();
+string str = "Hello, World!";
+Console.WriteLine($"원본 : {str}");
+Console.WriteLine();
+
+StringProcessor processor = ToUpperCase;
+Console.WriteLine($"[대문자 변환]");
+ProcessAndPrint(str, processor);
+Console.WriteLine();
+
+processor = ToLowerCase;
+Console.WriteLine($"[소문자 변환]");
+ProcessAndPrint(str, processor);
+Console.WriteLine();
+
+processor = AddBrackets;
+Console.WriteLine($"[괄호 추가]");
+ProcessAndPrint(str, processor);
+Console.WriteLine();
+
+processor = Reverse;
+Console.WriteLine($"[소문자 변환]");
+ProcessAndPrint(str, processor);
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine();
+
+delegate string StringProcessor(string str);
